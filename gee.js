e544d07fd6deb92f4f2ebcd879695df8a2cffb04
Kratos <64226667+thelifeQuest@users.noreply.github.com>
2020-06-09 14:06:48 +0200

    Update gee.js

--- gee.js
+++ gee.js
@@ -14,12 +14,22 @@ function nameChecker(){
     } else if(name == "Denis"  ||	name == "denis" ){
         document.getElementById("span2").innerText="You're worse! You're a fucking bully and a homophobe!!"; 
   }
+  else if(name == "Marco"  ||	name == "marco" ){
+    document.getElementById("span2").innerText="You're of a worse breed, you're a weeb."; 
+}
+else if(name == "Rinor"  ||	name == "rinor" ){
+    document.getElementById("span2").innerText="Naah bro, you're a halal kid."; 
+}
   else if(name == "cedi"  ||	name == "cedric"  ||	name == "Cedi"   ||	name == "Cedric" ){
     document.getElementById("span2").innerText="berfecccct"; 
 }
 
+else if(name == "Alessandro"  ||	name == "alessandro"  ||	name == "Alexhandro"   ||	name == "alexhandro" ){
+    document.getElementById("span2").innerText="You shove your guitar up your ass for fun, you gee ass nibba."; 
+}
+
 else if(name == "Thomas"  ||	name == "thomas"  ){
-    document.getElementById("span2").innerText="berfecccct"; 
+    document.getElementById("span2").innerText="NO COMMENT!"; 
 }
 else if(name == "" ){
     document.getElementById("span2").innerText="Type your name, dummy!"; 
